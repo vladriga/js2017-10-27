04d818c9d17de16e172e4a983b98f467d3e0a7db
mgrinko <glukym@gmail.com>
2017-12-12 19:51:57 +0200

    PhonesPage component was created

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+new PhonesPage({
+  element: document.querySelector('[data-component="phone-catalogue"]'),
+});
---
+++ public/js/phones-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class PhonesPage {
+  constructor({ element }) {
+    this._element = element;
+
+    alert(123123);
+  }
+}
